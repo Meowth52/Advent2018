3026af848f87389ffad37874ae8974ded9c2f39d
Meowth52 <meowth@meowth.se>
2018-12-05 07:59:32 +0100

    Day 4

--- Advent2018/Day04.cs
+++ Advent2018/Day04.cs
@@ -9,14 +9,67 @@ namespace Advent2018
 {
     public class Day04 : Day
     {
+        string[] Instructions;
         public Day04(string _input) : base(_input)
         {
-
+            Instructions = this.parseStringArray(_input);
         }
         public override Tuple<string, string> getResult()
         {
             int Sum = 0;
             int Sum2 = 0;
+            SortedDictionary<DateTime, string> SortedInstructions = new SortedDictionary<DateTime, string>();
+            foreach(string s in Instructions)
+            {
+                string[] SplitInstruction = s.Replace("[", "").Split(']');
+                SortedInstructions.Add(DateTime.Parse(SplitInstruction[0]), SplitInstruction[1]);
+            }
+            Dictionary<int, Guard> Guards = new Dictionary<int, Guard>();
+            int CurrentGuard = 0;
+            DateTime FeelAsleep= new DateTime(1, 1, 1, 0, 0, 0);
+            foreach(KeyValuePair<DateTime,string> k in SortedInstructions)
+            {
+                char Switcheroo = k.Value[1];
+                switch (Switcheroo)
+                {
+                    case 'G':
+                        string[] SplitGuard = k.Value.Replace("#", "").Split(' ');
+                        int TryInt = 0;
+                        Int32.TryParse(SplitGuard[2], out TryInt);
+                        CurrentGuard = TryInt;
+                        if (!Guards.ContainsKey(CurrentGuard))
+                            Guards.Add(CurrentGuard, new Guard());
+                        break;
+                    case 'w':
+                        Guards[CurrentGuard].fillMinutes(FeelAsleep, k.Key);
+                        break;
+                    case 'f':
+                        FeelAsleep = k.Key;
+                        break;
+                    default:
+                        break;
+                }             
+            }
+            int WinningGuard = 0;
+            int WinningSleep = 0;
+            int Part2WinningSleep = 0;
+            int Part2WinningGuard = 0;
+            foreach (KeyValuePair<int,Guard> k in Guards)
+            {
+                if (k.Value.getTotalTime() > WinningSleep)
+                {
+                    WinningSleep = k.Value.getTotalTime();
+                    WinningGuard = k.Key;
+                }
+                if (k.Value.getMostestMinuteAmount() > Part2WinningSleep)
+                {
+                    Part2WinningSleep = k.Value.getMostestMinuteAmount();
+                    Part2WinningGuard = k.Key;
+                }
+            }
+
+            Sum = WinningGuard * Guards[WinningGuard].getMostestMinute();
+            Sum2 = Part2WinningGuard * Guards[Part2WinningGuard].getMostestMinute();
             return Tuple.Create(Sum.ToString(), Sum2.ToString());
         }
         public override string getPartOne()
@@ -27,5 +80,59 @@ namespace Advent2018
         {
             throw new NotImplementedException();
         }
+    }
+    public class Guard
+    {
+        Dictionary<int, int> MinuteList = new Dictionary<int, int>();
+        int TotalTime = 0;
+        public Guard()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                MinuteList.Add(i, 0);
+            }
+        }
+        public void fillMinutes(DateTime from, DateTime to)
+        {
+            for(int i = from.Minute; i <= to.Minute; i++)
+            {
+                MinuteList[i]++;
+                TotalTime++;
+            }
+        }
+        public int getMostestMinute()
+        {
+            int MostesMinute = 0;
+            int minute = 0;
+            foreach(KeyValuePair<int,int> k in MinuteList)
+            {
+                if (k.Value > MostesMinute)
+                {
+                    MostesMinute = k.Value;
+                    minute = k.Key;
+                }
+            }
+            return minute;
+        }
+        public int getMostestMinuteAmount()
+        {
+            int MostesMinute = 0;
+            int minute = 0;
+            foreach (KeyValuePair<int, int> k in MinuteList)
+            {
+                if (k.Value > MostesMinute)
+                {
+                    MostesMinute = k.Value;
+                    minute = k.Key;
+                }
+            }
+            return MostesMinute;
+
+        }
+        public int getTotalTime()
+        {
+            return TotalTime;
+        }
+
     }
 }
--- Advent2018Tests/Day04Tests.cs
+++ Advent2018Tests/Day04Tests.cs
@@ -14,9 +14,9 @@ namespace Advent2018.Tests
         [TestMethod()]
         public void Day04Test()
         {
-            Day _day04 = new Day04("");
-            string PartOneExpected = "Fail";
-            string PartTwoExpected = "Fail";
+            Day _day04 = new Day04("[1518-11-01 00:00] Guard #10 begins shift\r\n[1518-11-01 00:05] falls asleep\r\n[1518-11-01 00:25] wakes up\r\n[1518-11-01 00:30] falls asleep\r\n[1518-11-01 00:55] wakes up\r\n[1518-11-01 23:58] Guard #99 begins shift\r\n[1518-11-02 00:40] falls asleep\r\n[1518-11-02 00:50] wakes up\r\n[1518-11-03 00:05] Guard #10 begins shift\r\n[1518-11-03 00:24] falls asleep\r\n[1518-11-03 00:29] wakes up\r\n[1518-11-04 00:02] Guard #99 begins shift\r\n[1518-11-04 00:36] falls asleep\r\n[1518-11-04 00:46] wakes up\r\n[1518-11-05 00:03] Guard #99 begins shift\r\n[1518-11-05 00:45] falls asleep\r\n[1518-11-05 00:55] wakes up");
+            string PartOneExpected = "240";
+            string PartTwoExpected = "4455";
             Tuple<string, string> Actual = _day04.getResult();
             Assert.AreEqual(PartOneExpected, Actual.Item1);
             Assert.AreEqual(PartTwoExpected, Actual.Item2);
