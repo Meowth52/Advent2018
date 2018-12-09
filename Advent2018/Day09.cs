b3cd8334c224bd7c321044dc2d2e160cf97fc556
David Granberg <meowth@meowth.se>
2018-12-09 15:18:20 +0100

    Day 9

--- Advent2018/Day09.cs
+++ Advent2018/Day09.cs
@@ -9,15 +9,65 @@ namespace Advent2018
 {
     public class Day09 : Day
     {
+        List<string[]> Instruction;
+        int NrOfPlayers = 0;
+        int LastMarble = 0;
         public Day09(string _input) : base(_input)
         {
-
+            Instruction = this.parseListOfStringArrays(_input);
         }
         public override Tuple<string, string> getResult()
         {
-            int Sum = 0;
-            int Sum2 = 0;
+            Int32.TryParse(Instruction[0][0], out NrOfPlayers);
+            Int32.TryParse(Instruction[0][6], out LastMarble);
+            long Sum = getPartOneInt();
+            LastMarble *= 100;
+            long Sum2 = getPartOneInt();
             return Tuple.Create(Sum.ToString(), Sum2.ToString());
+        }
+        public long getPartOneInt()
+        {
+            long Sum = 0;
+            LinkedList<int> TheCircle = new LinkedList<int>();
+            TheCircle.AddFirst(0);
+            TheCircle.AddLast(1);
+            LinkedListNode<int> Current = TheCircle.Last;
+            int CurrentPlayer = 2;
+            List<long> PlayerScores = new List<long>();
+            for (int i = 0; i <= NrOfPlayers; i++)
+            {
+                PlayerScores.Add(0);
+            }
+            for (int i = 2; i <= LastMarble; i++)
+            {
+                ;
+                if (i % 23 == 0)
+                {
+                    PlayerScores[CurrentPlayer] += i;
+                    pushTheIndex(ref Current, -7, false);
+                    PlayerScores[CurrentPlayer] += Current.Value;
+                    LinkedListNode<int> RemoveThis = Current;
+                    pushTheIndex(ref Current, 1, true);
+                    TheCircle.Remove(RemoveThis);
+                }
+                else
+                {
+                    pushTheIndex(ref Current, 1, true);
+                    TheCircle.AddAfter(Current, i);
+                    Current = Current.Next;
+                }
+                CurrentPlayer++;
+                if (CurrentPlayer > NrOfPlayers)
+                {
+                    CurrentPlayer = 1;
+                }
+            }
+            foreach (long i in PlayerScores)
+            {
+                if (i > Sum)
+                    Sum = i;
+            }
+            return Sum;
         }
         public override string getPartOne()
         {
@@ -26,6 +76,15 @@ namespace Advent2018
         public override string getPartTwo()
         {
             throw new NotImplementedException();
+        }
+        public void pushTheIndex(ref LinkedListNode<int> Current, int NrOfSteps, bool Positive)
+        {
+            if (Positive)
+                for (int i = 0; i < NrOfSteps; i++)
+                    Current = Current.Next ?? Current.List.First;
+            else
+                for (int i = 0; i > NrOfSteps; i--)
+                    Current = Current.Previous ?? Current.List.Last;
         }
     }
 }
--- Advent2018Tests/Day09Tests.cs
+++ Advent2018Tests/Day09Tests.cs
@@ -12,11 +12,61 @@ namespace Advent2018.Tests
     public class Day09Tests
     {
         [TestMethod()]
-        public void Day09Test()
+        public void Day09Test01()
         {
-            Day _day09 = new Day09("");
-            string PartOneExpected = "Fail";
-            string PartTwoExpected = "Fail";
+            Day _day09 = new Day09("9 players; last marble is worth 25 points");
+            string PartOneExpected = "32";
+            string PartTwoExpected = "0";
+            Tuple<string, string> Actual = _day09.getResult();
+            Assert.AreEqual(PartOneExpected, Actual.Item1);
+            Assert.AreEqual(PartTwoExpected, Actual.Item2);
+        }
+        [TestMethod()]
+        public void Day09Test02()
+        {
+            Day _day09 = new Day09("10 players; last marble is worth 1618 points");
+            string PartOneExpected = "8317";
+            string PartTwoExpected = "0";
+            Tuple<string, string> Actual = _day09.getResult();
+            Assert.AreEqual(PartOneExpected, Actual.Item1);
+            Assert.AreEqual(PartTwoExpected, Actual.Item2);
+        }
+        [TestMethod()]
+        public void Day09Test03()
+        {
+            Day _day09 = new Day09("13 players; last marble is worth 7999 points");
+            string PartOneExpected = "146373";
+            string PartTwoExpected = "0";
+            Tuple<string, string> Actual = _day09.getResult();
+            Assert.AreEqual(PartOneExpected, Actual.Item1);
+            Assert.AreEqual(PartTwoExpected, Actual.Item2);
+        }
+        [TestMethod()]
+        public void Day09Test04()
+        {
+            Day _day09 = new Day09("17 players; last marble is worth 1104 points");
+            string PartOneExpected = "2764";
+            string PartTwoExpected = "0";
+            Tuple<string, string> Actual = _day09.getResult();
+            Assert.AreEqual(PartOneExpected, Actual.Item1);
+            Assert.AreEqual(PartTwoExpected, Actual.Item2);
+        }
+        [TestMethod()]
+        public void Day09Test05()
+        {
+            Day _day09 = new Day09("21 players; last marble is worth 6111 points");
+            string PartOneExpected = "54718";
+            string PartTwoExpected = "0";
+            Tuple<string, string> Actual = _day09.getResult();
+            Assert.AreEqual(PartOneExpected, Actual.Item1);
+            Assert.AreEqual(PartTwoExpected, Actual.Item2);
+        }
+        [TestMethod()]
+        public void Day09Test06()
+        {
+            Day _day09 = new Day09("30 players; last marble is worth 5807 points");
+            string PartOneExpected = "37305";
+            string PartTwoExpected = "0";
             Tuple<string, string> Actual = _day09.getResult();
             Assert.AreEqual(PartOneExpected, Actual.Item1);
             Assert.AreEqual(PartTwoExpected, Actual.Item2);
