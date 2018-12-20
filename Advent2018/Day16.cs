e60cb1d62991c3d5b5ad36785edc7923babdf1d3
Meowth52 <meowth@meowth.se>
2018-12-20 19:46:49 +0100

    Day 16 done

--- Advent2018/Day16.cs
+++ Advent2018/Day16.cs
@@ -16,7 +16,7 @@ namespace Advent2018
             string Input = _input.Replace("\r\n\r\n", "_");
             string[] SplitString = Input.Split('_');
             Instructions1 = new List<List<List<int>>>();
-            foreach(string s in SplitString)
+            foreach (string s in SplitString)
             {
                 if (!string.IsNullOrWhiteSpace(s))
                 {
@@ -35,11 +35,102 @@ namespace Advent2018
         {
             int Sum = 0;
             int Sum2 = 0;
-            List<int> Registers = new List<int>();
-            foreach(List<List<int>> ListList in Instructions1)
+            Dictionary<string, List<int>> OpCodes = new Dictionary<string, List<int>>
             {
-                Registers = ListList[0];
+                {"addr", new List<int>()},
+                {"addi", new List<int>()},
+                {"mulr", new List<int>()},
+                {"muli", new List<int>()},
+                {"banr", new List<int>()},
+                {"bani", new List<int>()},
+                {"borr", new List<int>()},
+                {"bori", new List<int>()},
+                {"setr", new List<int>()},
+                {"seti", new List<int>()},
+                {"gtir", new List<int>()},
+                {"gtri", new List<int>()},
+                {"gtrr", new List<int>()},
+                {"eqir", new List<int>()},
+                {"eqri", new List<int>()},
+                {"eqrr", new List<int>()},
+            };
+            //Part 1
+            foreach (List<List<int>> ListList in Instructions1)
+            {
+                int SuccessCounter = 0;
+                foreach (KeyValuePair<string, List<int>> OpCode in OpCodes)
+                {
+                    if (ListList[2].SequenceEqual(Operate(OpCode.Key, ListList[1], ListList[0])))
+                    {
+                        SuccessCounter++;
+                    }
+                }
+                if (SuccessCounter >= 3)
+                    Sum++;
             }
+            //Part 2
+            foreach (List<List<int>> ListList in Instructions1)
+            {
+                List<string> Edits = new List<string>();
+                foreach (KeyValuePair<string, List<int>> OpCode in OpCodes)
+                {
+                    if (!OpCode.Value.Contains(ListList[1][0]))
+                    {
+                        if (ListList[2].SequenceEqual(Operate(OpCode.Key, ListList[1], ListList[0])))
+                        {
+                            ;
+                        }
+                        else
+                        {
+                            Edits.Add(OpCode.Key);
+                        }
+
+                    }
+                }
+                foreach (string s in Edits)
+                {
+                    if (!OpCodes[s].Contains(ListList[1][0]))
+                        OpCodes[s].Add(ListList[1][0]);
+                }
+            }
+            Dictionary<int,string> DecodedCodes = new Dictionary<int,string>();
+            while (DecodedCodes.Count < 16)
+            {
+                int RemovedInt = -1;
+                string RemoveThis = "";
+
+                foreach (KeyValuePair<string, List<int>> k in OpCodes)
+                {
+                    if (!DecodedCodes.ContainsValue(k.Key) && k.Value.Count == 15)
+                    {
+                        for (int i = 0; i < 16; i++)
+                        {
+                            if (!k.Value.Contains(i))
+                            {
+                                DecodedCodes.Add(i, k.Key);
+                                RemovedInt = i;
+                                RemoveThis = k.Key;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                OpCodes.Remove(RemoveThis);
+                foreach (KeyValuePair<string, List<int>> k in OpCodes)
+                {
+                    if (!k.Value.Contains(RemovedInt))
+                    {
+                        k.Value.Add(RemovedInt);
+                    }
+                }
+            }
+            List<int> Registers = new List<int>() {0,0,0,0};
+            foreach (List<int> l in Instructions2)
+            {
+                Registers = Operate(DecodedCodes[l[0]], l, Registers);
+            }
+            Sum2 = Registers[0];
             return Tuple.Create(Sum.ToString(), Sum2.ToString());
         }
         public override string getPartOne()
@@ -50,8 +141,84 @@ namespace Advent2018
         {
             throw new NotImplementedException();
         }
-        public int[] GetOperationResult(int OpCode,int[] Registers)
+        public List<int> Operate(string OpCode, List<int> OpNumbers, List<int> _registers)
         {
+            List<int> Registers = new List<int>(_registers);
+            int A = OpNumbers[1];
+            int B = OpNumbers[2];
+            int C = OpNumbers[3];
+            switch (OpCode)
+            {
+                case "addr":
+                    Registers[C] = Registers[A] + Registers[B];
+                    break;
+                case "addi":
+                    Registers[C] = Registers[A] + B;
+                    break;
+                case "mulr":
+                    Registers[C] = Registers[A] * Registers[B];
+                    break;
+                case "muli":
+                    Registers[C] = Registers[A] * B;
+                    break;
+                case "banr":
+                    Registers[C] = Registers[A] & Registers[B];
+                    break;
+                case "bani":
+                    Registers[C] = Registers[A] & B;
+                    break;
+                case "borr":
+                    Registers[C] = Registers[A] | Registers[B];
+                    break;
+                case "bori":
+                    Registers[C] = Registers[A] | B;
+                    break;
+                case "setr":
+                    Registers[C] = Registers[A];
+                    break;
+                case "seti":
+                    Registers[C] = A;
+                    break;
+                case "gtir":
+                    if (A > Registers[B])
+                        Registers[C] = 1;
+                    else
+                        Registers[C] = 0;
+                    break;
+                case "gtri":
+                    if (Registers[A] > B)
+                        Registers[C] = 1;
+                    else
+                        Registers[C] = 0;
+                    break;
+                case "gtrr":
+                    if (Registers[A] > Registers[B])
+                        Registers[C] = 1;
+                    else
+                        Registers[C] = 0;
+                    break;
+                case "eqir":
+                    if (A == Registers[B])
+                        Registers[C] = 1;
+                    else
+                        Registers[C] = 0;
+                    break;
+                case "eqri":
+                    if (Registers[A] == B)
+                        Registers[C] = 1;
+                    else
+                        Registers[C] = 0;
+                    break;
+                case "eqrr":
+                    if (Registers[A] == Registers[B])
+                        Registers[C] = 1;
+                    else
+                        Registers[C] = 0;
+                    break;
+                default:
+                    break;
+            }
+
             return Registers;
         }
     }
--- Advent2018/MainWindow.xaml.cs
+++ Advent2018/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace Advent2018
 {
     public partial class MainWindow : Window
     {
-        int LastDay = 19;
+        int LastDay = 16;
         public int ChoosenDay;
         private readonly MainView _mainView;
         public MainWindow()
