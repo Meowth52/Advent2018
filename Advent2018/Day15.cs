76051a44ab7a02126279946041cc6c191232373d
Meowth52 <meowth@meowth.se>
2019-08-15 16:31:00 +0200

    Update Day15.cs

--- Advent2018/Day15.cs
+++ Advent2018/Day15.cs
@@ -11,6 +11,8 @@ namespace Advent2018
     {
         string[] Instructions;
         string[,] Map;
+        Dictionary<string, Fighter> Elves;
+        Dictionary<string, Fighter> Goblins;
         public Day15(string _input) : base(_input)
         {
             Instructions = this.parseStringArray(_input);
@@ -27,6 +29,26 @@ namespace Advent2018
                 y = 0;
                 x += 1;
             }
+            Elves = new Dictionary<string, Fighter>();
+            Goblins = new Dictionary<string, Fighter>();
+            int DictionaryIndex = 0;
+            for(x = 0; x < Map.GetLength(0); x++)
+            {
+                for (y = 0; y < Map.GetLength(0); y++)
+                {
+                    if (Map[x,y] == "E")
+                    {
+                        Map[x, y] = Map[x, y] + DictionaryIndex.ToString();
+                        Elves.Add(Map[x, y], new Fighter(3, 200,x,y));
+                    }
+                    else if (Map[x, y] == "G")
+                    {
+                        Map[x, y] = Map[x, y] + DictionaryIndex.ToString();
+                        Goblins.Add(Map[x, y], new Fighter(3, 200,x,y));
+                    }
+                    DictionaryIndex++;
+                }
+            }
         }
         public override Tuple<string, string> getResult()
         {
@@ -35,15 +57,65 @@ namespace Advent2018
             string Returnstring = "";
             foreach (string s in Map)
                 Returnstring += s;
-            return Tuple.Create(Returnstring, Sum2.ToString());
+            return Tuple.Create(getPartOne(), Sum2.ToString());
         }
         public override string getPartOne()
         {
+            while (Elves.Count > 0 && Goblins.Count > 0)
+            {
+                for (int x = 0; x < Map.GetLength(0); x++)
+                {
+                    for (int y = 0; y < Map.GetLength(0); y++)
+                    {
+                        if (Map[x, y][0] == 'E')
+                        {
+
+                        }
+                        else if (Map[x, y][0] == 'G')
+                        {
+
+                        }
+                    }
+                }
+
+            }
             throw new NotImplementedException();
         }
         public override string getPartTwo()
         {
             throw new NotImplementedException();
         }
+    }
+    public class Fighter
+    {
+        int AttackPower;
+        int Hp;
+        Coordinate Position;
+        public Fighter(int attackPower, int hp, int x, int y)
+        {
+            AttackPower = attackPower;
+            Hp = hp;
+            Position = new Coordinate(x, y);
+        }
+        public Coordinate GetClosestTarget()
+        {            
+            return ;
+        }
+        public Coordinate GetNextSquare()
+        {
+            return;
+        }
+        public List<Coordinate> GetFreeNeighbours(string[,] map)
+        {
+            List<Coordinate> directions = new List<Coordinate>();
+            directions.Add(new Coordinate(1,0));
+            directions.Add(new Coordinate(-1, 0));
+            directions.Add(new Coordinate(0, 1));
+            directions.Add(new Coordinate(0, -1));
+            List<Coordinate> returnList = new List<Coordinate>();
+
+            return;
+        }
+
     }
 }
